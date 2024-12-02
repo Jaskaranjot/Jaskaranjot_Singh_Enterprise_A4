bc1a70a536069e7f92b749ada5a8fe52db21f828
Jaskaranjot Singh <jaskaranjot11@gmail.com>
2024-12-02 18:41:38 -0500

    Add project files.

---
+++ Jaskaranjot_Singh/Models/Income.cs
@@ -0,0 +1,11 @@
+﻿namespace Jaskaranjot_Singh.Models
+{
+    public class Income
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public double Amount { get; set; }
+        public bool IsReceived { get; set; }
+        public int UserId { get; set; }
+    }
+}
