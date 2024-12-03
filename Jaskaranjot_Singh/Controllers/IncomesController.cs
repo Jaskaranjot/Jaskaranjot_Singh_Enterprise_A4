072c7867f011769e3cd58ee9713e315bb3f3136a
Jaskaranjot Singh <jaskaranjot11@gmail.com>
2024-12-02 21:38:45 -0500

    Jaskaranjot: 21:38, work is done

---
+++ Jaskaranjot_Singh/Controllers/IncomesController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Jaskaranjot_Singh.Models;
+
+namespace Jaskaranjot_Singh.Controllers
+{
+    [ApiController]
+[Route("api/[controller]")]
+public class IncomesController : ControllerBase
+{
+    [HttpGet("{userId}")]
+    public IActionResult GetTotalIncome(int userId)
+    {
+        var totalIncome = IncomeRepository.GetTotalIncome(userId);
+        return Ok(totalIncome);
+    }
+
+    [HttpGet("{userId}/{isReceived}")]
+    public IActionResult GetReceivedIncomes(int userId, bool isReceived)
+    {
+        var incomes = IncomeRepository.GetReceivedIncomes(userId, isReceived);
+        return Ok(incomes);
+    }
+
+    [HttpDelete("{userId}")]
+    public IActionResult DeleteIncomes(int userId)
+    {
+        IncomeRepository.DeleteIncomes(userId);
+        return NoContent();
+    }
+
+    [HttpPost]
+    public IActionResult AddIncome([FromBody] Income income)
+    {
+        if (string.IsNullOrEmpty(income.Description) || income.Amount <= 0)
+        {
+            return BadRequest("Invalid income data.");
+        }
+
+        var newIncome = IncomeRepository.AddIncome(income);
+        return CreatedAtAction(nameof(GetTotalIncome), new { userId = newIncome.UserId }, newIncome);
+    }
+}
+}
---
+++ Jaskaranjot_Singh/IncomeRespository.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Jaskaranjot_Singh.Models;
+
+namespace Jaskaranjot_Singh
+{
+    public static class IncomeRepository
+    {
+        private static readonly List<Income> Incomes = new();
+
+        public static double GetTotalIncome(int userId)
+        {
+            return Incomes.Where(i => i.UserId == userId).Sum(i => i.Amount);
+        }
+
+        public static IEnumerable<Income> GetReceivedIncomes(int userId, bool isReceived)
+        {
+            return Incomes.Where(i => i.UserId == userId && i.IsReceived == isReceived);
+        }
+
+        public static void DeleteIncomes(int userId)
+        {
+            Incomes.RemoveAll(i => i.UserId == userId);
+        }
+
+        public static Income AddIncome(Income income)
+        {
+            income.Id = Incomes.Any() ? Incomes.Max(i => i.Id) + 1 : 1;
+            Incomes.Add(income);
+            return income;
+        }
+    }
+}
